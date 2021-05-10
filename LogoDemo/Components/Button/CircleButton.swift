896042bdc2ed5c46f70df3bef7e8e1d5922e77bb
Terry <terrykuo501@gmail.com>
2021-05-10 15:45:11 +0800

    add gitignorefile

---
+++ LogoDemo/Components/Button/CircleButton.swift
@@ -0,0 +1,20 @@
+//
+//  CircleButton.swift
+//  LogoDemo
+//
+//  Created by Terry Kuo on 2021/5/10.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
